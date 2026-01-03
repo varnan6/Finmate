2e8593291efee01ea470a8d55c4730d1e826c8d7
Varnan Rathod <varnanrathod6@gmail.com>
2026-01-03 17:59:58 +0530

    Update api base url in frontend

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE_URL = "https://finmate-api-wbp9.onrender.com/api";
+const API_BASE_URL = "https://finmate-api-wbp9.onrender.com";
 
 const api = axios.create({
   baseURL: API_BASE_URL,
