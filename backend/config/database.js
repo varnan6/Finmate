8bd01f53a44cea375cc72e76edc7d487f14ee692
Varnan Rathod <varnanrathod6@gmail.com>
2026-01-03 18:41:14 +0530

    Fixed URL variable

--- backend/config/database.js
+++ backend/config/database.js
@@ -2,7 +2,7 @@ const { Pool } = require("pg");
 require("dotenv").config();
 
 const pool = new Pool({
-  connectionString: process.env.SUPABASE_DB_URL,
+  connectionString: process.env.DATABASE_URL,
   ssl: {
     rejectUnauthorized: false,
   },
